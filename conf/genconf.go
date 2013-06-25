c219ed27806956c66db75a4efc410e38620b84b1
Jay Booth <jaybooth@gmail.com>
2013-06-24 20:36:20 -0400

    added sample configs and generator

--- conf/genconf.go
+++ conf/genconf.go
@@ -31,8 +31,16 @@ func main() {
 	}
 	
 	dscfg := integration.DSConfig{}
+	port := 12101
 	for i := 0 ; i < 4 ; i++ {
-		
+		dscfg.NameAddr = nnbind
+		dscfg.LeaseAddr = lsbind
+		dscfg.DataClientBindAddr = fmt.Sprintf("0.0.0.0:%d",port)
+		port++
+		dscfg.NameDataBindAddr = fmt.Sprintf("0.0.0.0:%d",port)
+		port += 100
+		dscfg.VolumeRoots = []string { fmt.Sprintf("/tmp/data%d-0",i), fmt.Sprintf("/tmp/data%d-1",i) }
+    dscfg.Write(fmt.Sprintf("%s/dn%d.cfg",baseDir,i))
 	}
 }
 
